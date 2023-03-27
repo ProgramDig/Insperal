828425a96d32062d5e0abdc6f41cabb15dd10e5c
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-03-27 14:15:19 +0300

    (feat) - add create and get all methood

--- src/women-accounting/women-accounting.controller.ts
+++ src/women-accounting/women-accounting.controller.ts
@@ -1,5 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateWADto } from "./dto/Create-WA.dto";
+import { WomenAccountingService } from "./women-accounting.service";
 
 @Controller("women-accounting")
 export class WomenAccountingController {
+
+  constructor(private womenAccountingService: WomenAccountingService) {
+  }
+
+  @Post()
+  createEntity(@Body() dto: CreateWADto){
+    return this.womenAccountingService.create(dto)
+  }
+
+  @Get()
+  getAllEntities(){
+    return this.womenAccountingService.getAll()
+  }
 }
--- src/women-accounting/women-accounting.service.ts
+++ src/women-accounting/women-accounting.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { CreateWADto } from "./dto/Create-WA.dto";
+import { InjectModel } from "@nestjs/sequelize";
+import { WomenAccounting } from "./women-accounting.model";
 
 @Injectable()
-export class WomenAccountingService {}
+export class WomenAccountingService {
+
+  constructor(@InjectModel(WomenAccounting) private womenAccountRepository: typeof WomenAccounting) {
+  }
+
+  async create(dto: CreateWADto) {
+    console.log(dto)
+    const woman = await this.womenAccountRepository.create(dto);
+    return woman;
+  }
+
+  async getAll() {
+    const women = await this.womenAccountRepository.findAll();
+    return women;
+  }
+}
