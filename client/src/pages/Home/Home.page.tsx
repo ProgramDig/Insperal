8fc2229edef6ea3350c9691fac31fe6ba562f558
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-03-29 16:36:14 +0300

    (feat) - edit home page

--- client/src/pages/Home/Home.page.tsx
+++ client/src/pages/Home/Home.page.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import classes from "*.module.scss";
+import logo from "../../assets/box-fill-black.svg";
+import classes from "./Home.page.module.scss";
+import { Button } from "react-bootstrap";
+import { url } from "../../main";
+import MyChart from "../../components/MyChart/MyChart";
 
-const HomePage = () => {
+const HomePage: React.FC = (): JSX.Element => {
   return (
     <div className={"container"}>
-      <h1>Home</h1>
+      <div className={classes.logoBlock}>
+        <h1 className={classes.title}><b>Insperal</b> <img src={logo} alt="logo" /></h1>
+        <div className={classes.text}>
+          <b>
+            Програмний модуль обліку резерву військово-навчених
+            ресурсів районного територіального центру
+            комплектування та соціальної підтримки для жінок
+          </b>
+        </div>
+        <div className={classes.version}>
+          <span>
+            <b>Версія</b> 1.0.0
+          </span>
+        </div>
+        <Button>
+          <a className={"nav-link"} href={`${url}/women-accounting`} target="_blank" >API бази даних</a>
+        </Button>
+      </div>
+
+      <div>
+        <h2 className={"mt-5 mb-5 d-flex justify-content-center"}>Графік активності використання додатку</h2>
+        <MyChart/>
+      </div>
     </div>
   );
 };
